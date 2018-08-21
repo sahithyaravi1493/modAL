fc83910f4f629c0387967c2a7104621a0b1faf39
cosmic-cortex <theodore.danka@gmail.com>
2018-08-21 16:19:31 +0200

    indexing with lists added, __getitem__ fixed

--- modAL/utils/optimization/search_space.py
+++ modAL/utils/optimization/search_space.py
@@ -9,7 +9,15 @@ class SearchSpace:
         self.var_names = list(kwargs.keys())
 
     def __getitem__(self, idx):
-        return self.space[idx]()
+        try:
+            item = self.space[idx]()
+        except TypeError:
+            if isinstance(idx, list):
+                item = np.array([self.space[i]() for i in idx])
+            else:
+                raise TypeError('SearchSpace indices must be integers or lists')
+
+        return item
 
     def numpy_sample(self):
         return np.array([dist() for dist in self.space])
