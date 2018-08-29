4ee1586ad1884ae61b22b590fd65a739e52587fc
cosmic-cortex <theodore.danka@gmail.com>
2018-08-29 14:50:34 +0200

    optimization interface added

---
+++ modAL/utils/optimization/optimizer.py
@@ -0,0 +1,11 @@
+"""
+Optimizer interface for query synthesis and acquisition function optimization.
+"""
+
+
+class Optimizer:
+    def __init__(self, optimizer_func, *opt_args, **opt_kwargs):
+        pass
+
+    def __call__(self, *args, **kwargsx):
+        pass
